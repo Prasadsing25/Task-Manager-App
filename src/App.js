10d8b7668c545614a8a6d6f40441c744fdc7fa74
Prasadsing Solanki <prasadsingsolanki6006@gmail.com>
2026-01-25 15:06:35 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [todolist, setTodolist] = useState([])
+
+  let saveTodoList = (event) => {
+    let todoname = event.target.todoname.value;
+    if (!todolist.includes(todoname)) {
+      let finalTodolist = [...todolist, todoname];
+      setTodolist(finalTodolist);
+    } else {
+      console.log("Alerady Existed");
+    }
+    event.preventDefault();
+  }
+
+  let list = todolist.map((value, index) => {
+    return (
+      <TodoListItems value={value} key={index} indexNumber={index}
+        todolist ={todolist}
+        setTodolist={setTodolist}
+      />
+    )
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <form onSubmit={saveTodoList}>
+        <input type="text" name='todoname' />
+        <button>Save</button>
+      </form>
+
+      <div className='outerDiv'>
+        <ul>
+          {list}
+        </ul>
+        <p>*complete your task on prior basis*</p>
+      </div>
     </div>
+  );
+}
+
+function TodoListItems({ value, indexNumber, todolist, setTodolist }) {
+  let [status, setStatus] = useState(false);
+
+  let deleteRow = () => {
+    // alert(indexNumber);
+    let finalData = todolist.filter((v, i) => i !== indexNumber);
+    setTodolist(finalData);
+  }
+
+  let checkStatus = () => {
+    setStatus(!status);
+  }
+
+  return (
+    <li className={(status) ? 'completetodo' : ''} onClick={checkStatus}>{indexNumber+1}. &nbsp;{value} <span onClick={deleteRow}>&times;</span></li>
   );
 }
 
